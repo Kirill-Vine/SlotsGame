2df3df96a1276c248e8fb8bfc9ad5ddc5ee632dd
Kirill Vinogradov <61331094+Kirill-Vine@users.noreply.github.com>
2023-08-12 13:15:29 -0400

    Initial Commit

---
+++ SlotsGame/SlotsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotsGameApp.swift
+//  SlotsGame
+//
+//  Created by kv on 8/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SlotsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
